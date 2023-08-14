f8f10b701ce9413262454bcde20ac6355e013a00
Tim Rivoli <36770881+TimRivoli@users.noreply.github.com>
2023-08-14 00:02:33 -0700

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import openai, constants 
-
 app = Flask(__name__)
 openai.api_key = constants.openai_api_key
-question_history = []  #keeps track of previous questions, the API doesn't remember prior questions asked, the aggregate-questions checkbox will submit all your prior questions with each question
+question_history = [] #keeps track of previous questions, the API doesn't remember prior questions asked, the aggregate-questions checkbox will submit all your prior questions with each question
+token_limit = 4096 	  #GPT 3.5 turbo limit, 4.0 is much higher
+response_tokens = 600 #How many tokens do you want to save for the response?  max_tokens = messages + response
 
 def ask_chatgpt(question:str, system_command:str = "", model:str = 'gpt-3.5-turbo', temperature:float=.1, max_tokens:int=1200):
 	openai.api_key = constants.openai_api_key
@@ -35,12 +36,13 @@ def get_response():
 			question = ''
 			for q in question_history:
 				if not "?" in q: q += "?"
-				question += q + " "
+				question += q + " "			
 		else:
 			question_history.clear()
+		if len(question) + response_tokens > token_limit: question = question[token_limit-response_tokens:]
 		print(f"Question: {question}")
 		gpt_system = "Format response in HTML"
-		response = ask_chatgpt(question, gpt_system)
+		response = ask_chatgpt(question, gpt_system, max_tokens=(len(question)+response_tokens))
 		return jsonify({'response': response})
 
 if __name__ == '__main__':
