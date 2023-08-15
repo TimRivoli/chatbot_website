6058e270430dfd54d5cfb1a9c4e45ffa03538558
Tim Rivoli <36770881+TimRivoli@users.noreply.github.com>
2023-08-14 19:32:15 -0700

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -3,37 +3,50 @@ const userInput = document.getElementById('user-input');
 const sendButton = document.getElementById('send-button');
 const clearButton = document.getElementById('clear-button');
 const copyButton = document.getElementById('copy-button');
-const aggregateQuestions = document.getElementById('aggregate-questions'); 
+const conversationMode = document.getElementById('conversation-mode'); 
+const responseSize = document.getElementById('response-size'); 
+const temperature = document.getElementById('temperature'); 
+const customInstructions = document.getElementById('user-instructions'); 
 
 sendButton.addEventListener('click', async () => {
     const userMessage = userInput.value;
     chatBox.innerHTML += `<div class="user-message">${userMessage}</div>`;
     userInput.value = '';
-
-    const formData = new URLSearchParams({
-        user_input: userMessage,
-        aggregate_questions: aggregateQuestions.checked 
-    });
-
-    const response = await fetch('/get_response', {
-        method: 'POST',
-        body: formData,
-    }).then(response => response.json());
-
+    const formData = new URLSearchParams({user_input: userMessage, conversation_mode: conversationMode.checked, response_size: responseSize.value, temperature: temperature.value, user_instructions: customInstructions.value});
+    const response = await fetch('/get_response', {method: 'POST', body: formData,}).then(response => response.json());
     chatBox.innerHTML += `<div class="bot-message">${response.response}</div>`;
     chatBox.scrollTop = chatBox.scrollHeight;
 });
+	
+clearButton.addEventListener('click', () => { 
+            fetch('/clear_history', {
+                method: 'POST'
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message);
+				chatBox.innerHTML = '';
 
-clearButton.addEventListener('click', () => {    chatBox.innerHTML = '';});
+            })
+            .catch(error => console.error('Error:', error));
+        });
 
 copyButton.addEventListener('click', () => {
     const botMessages = document.querySelectorAll('.bot-message');
-    const textToCopy = Array.from(botMessages).map(message => message.textContent).join('\n');
-    
+    const textToCopy = Array.from(botMessages).map(message => message.textContent).join('\n');   
     const tempTextarea = document.createElement('textarea');
     tempTextarea.value = textToCopy;
     document.body.appendChild(tempTextarea);
     tempTextarea.select();
     document.execCommand('copy');
     document.body.removeChild(tempTextarea);
+});
+
+
+
+userInput.addEventListener('keydown', event => {
+	if (event.key === 'Enter') {
+		event.preventDefault(); 
+		sendButton.click(); 
+	}
 });
