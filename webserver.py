098c531a1da18d4ea92062624e4089dff28547b0
Tim Rivoli <36770881+TimRivoli@users.noreply.github.com>
2023-08-15 22:04:08 -0700

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -1,35 +1,37 @@
 from flask import Flask, render_template, request, jsonify, session
-import openai, constants, tiktoken
+import openai, constants, tiktoken, uuid
 
 app = Flask(__name__)
 app.secret_key = 'shamalamabingbam_yougotit'
 openai.api_key = constants.openai_api_key
 token_limit = 4096 	  #GPT 3.5 turbo limit, 4.0 is much higher
-
-#the API doesn't remember prior questions and answers, the conversation_mode checkbox will submit the conversation history to the model which provides context but uses more tokens
-#session variables
-#'question_history' list to track previous questions
-#'response_history' list to track previous responses
-#'conversation_summary' string conversation summarized as needed to reduce the size of the messages
 max_question_length = 200  		   #Trim the history to this number of characters
 max_history_response_length = 750  #Trim the history to this number of characters
 openai.api_key = constants.openai_api_key
+user_list = []
+conversation_history = {} #Stores conversation history for each active user
+conversation_summary = {}   #Stores conversation summary for each active user
 
+# ------------------------------------------------------- API Calls ---------------------------------------------------------
 def _chatgpt_query(messages:list, response_tokens:int=1000, temperature:float=.2, include_usage_summary:bool=False, model:str = 'gpt-3.5-turbo'):
 	try: 
-		result = openai.ChatCompletion.create(model=model, messages=messages, temperature=temperature, max_tokens=response_tokens)
+		model_result = openai.ChatCompletion.create(model=model, messages=messages, temperature=temperature, max_tokens=response_tokens)
 		success = True
 	except Exception as e:
 		print("ChatGPT query failed:", e)
 		success = False
 		result = "API Call Failed"
 	if success: 
-		usage_summary = f"Usage Summary (Prompt: {result.usage.prompt_tokens} - Response: {result.usage.completion_tokens} - Total Tokens: {result.usage.total_tokens} - Response Limit: {response_tokens}  - Specified Model Limit: {token_limit})"
+		usage_summary = f"Usage Summary (Prompt: {model_result.usage.prompt_tokens}, Response: {model_result.usage.completion_tokens}, Total Tokens: {model_result.usage.total_tokens}, Response Limit: {response_tokens}, Specified Model Limit: {token_limit})"
 		print(usage_summary)
-		result = result.choices[0].message.content
-		session['response_history'].append(result[:max_history_response_length])
+		result = model_result.choices[0].message.content
+		x = result[:max_history_response_length]
+		token_count = _token_count(x)
+		_converstation_append(content=x, token_count=token_count, is_response=True)
 		result = result.replace("\n", "<BR>") 
-		result += "<BR>" + usage_summary
+		result = result.replace("<BR><BR>", "<BR>") 
+		result = result.replace("<BR><BR>", "<BR>") 
+		if include_usage_summary: result += "<BR>" + usage_summary
 	return result
 	
 def _token_count(text:str):
@@ -37,51 +39,85 @@ def _token_count(text:str):
 	if text:
 		encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 		token_count = len(encoding.encode(text))
-		print(f"The text contains {token_count} tokens.")
+		print(f" The text contains {token_count} tokens.")
 	return token_count
 
+# ------------------------------------------------ User Session Management  -------------------------------------------------
 def _session_init():
-	if not 'question_history' in session:
-		print('Session initialized.')
-		session['conversation_summary'] = ''
-		session['question_history'] = []
-		session['response_history'] = []
-		session['converstation_length'] = 0
+	if not 'uid' in session:
+		user_id = uuid.uuid4()
+		session['uid'] = user_id
+		user_list.append(user_id)
+		session.modified = True
+		print(' Session created: ', user_id)
+	else:
+		user_id = session['uid']
+	if not user_id in conversation_history:
+		conversation_history[user_id] = []
+		conversation_summary[user_id] = {"content": "", "tokens": 0}
+		print(' Session initialized: ', user_id)
 
-def _converstation_history_clear():
-	print('Clearning chat history')
-	session['conversation_summary'] = ''
-	session['question_history'].clear()
-	session['response_history'].clear()
+def _converstation_clear():
+	conversation_history[session['uid']] = []
+	conversation_summary[session['uid']] = {"content": "", "tokens": 0}
+	print(' Chat history cleared: ', session['uid'])
+
+def _converstation_append(content:str, token_count:int, is_response:bool):
+	if len(content) > 0:
+		if is_response:
+			conversation_history[session['uid']].append({"role": "assistant", "content": content[:max_history_response_length], "tokens": token_count})
+		else:
+			conversation_history[session['uid']].append({"role": "user", "content": content[:max_question_length], "tokens": token_count})		
 
 def _converstation_summarize(messages:list):
-	print('Summarizing converstation...')
-	session['conversation_summary'] = _chatgpt_query(messages=messages, response_tokens=1000, temperature=0.0)
-	print(session['conversation_summary'])
+	print(' Summarizing converstation...')
+	content = _chatgpt_query(messages=messages, response_tokens=1000, temperature=0.0)
+	token_count = _token_count(content)
+	conversation_summary[session['uid']]  = {"content": content, "tokens": token_count}
+	while len(conversation_history[session['uid']]) > 50:
+		x = conversation_history[session['uid']].pop(0)
+	for i in range(len(conversation_history[session['uid']])):
+		x = conversation_history[session['uid']][i]
+		tokens = x['tokens']
+		if x['role'] == "assistant" and tokens > 25:
+			content = x['content']
+			if tokens > 500:
+				content = content[:500]
+			elif x['tokens'] > 100:
+				content = content[:100]
+			elif x['tokens'] > 50:
+				content = content[:50]
+			elif x['tokens'] > 25:
+				content = content[:25]
+			tokens = _token_count(content)
+			conversation_history[session['uid']][i]['content'] = content
+			conversation_history[session['uid']][i]['tokens'] = tokens			
+	print(' Conversation Summary: ', content)
 
 def _converstation_history_get(tokens_spoken_for:int = 0):
 	messages = []
-	#print(len(session['question_history']), len(session['response_history']))
-	while len(session['question_history']) < len(session['response_history']): session['response_history'].append("") #These could get out of sync with asyc calls if the user spam clicks
-	while len(session['response_history']) > len(session['question_history']): session['response_history'].pop(0)	
-	#print(len(session['question_history']), len(session['response_history']))
-	if session['conversation_summary'] !='': 
-		tokens_spoken_for += _token_count(session['conversation_summary'])
-		messages.append({"role": "system", "content": 'Summary of our conversation so far: ' + session['conversation_summary']})
+	if conversation_summary[session['uid']]['content'] !='': 
+		tokens_spoken_for += conversation_summary[session['uid']]['tokens']
+		messages.append({"role": "system", "content": 'Conversation summary: ' + conversation_summary[session['uid']]['content']})
 	tokens_estimated = tokens_spoken_for
-	for i in range(len(session['question_history'])):
-		q = session['question_history'][i]
-		a = session['response_history'][i]
-		messages.append({"role": "user", "content": q})
-		messages.append({"role": "assistant", "content": a})
-		tokens_estimated += _token_count(q +' '+ a)
-	if tokens_estimated  > token_limit -400 and len(messages) > 1:
-		while tokens_estimated  > token_limit and len(messages) > 1:
+	for m in conversation_history[session['uid']]:
+		messages.append({"role": m['role'], "content": m['content']})
+		tokens_estimated += m['tokens']
+	if tokens_estimated > token_limit/3 and len(messages) > 1:
+		print('Trimming and summarizing conversation..')
+		if 'Summary' in messages[0]['content']: 
+			x = messages.pop(0)
+			tokens_spoken_for -= conversation_summary[session['uid']]['tokens']
+		_converstation_summarize(messages)
+		tokens_spoken_for += conversation_summary[session['uid']]['tokens']
+		messages.insert(0, {"role": "system", "content": 'Conversation summary: ' + conversation_summary[session['uid']]['content']})
+		while tokens_estimated > token_limit and len(messages) > 1:
 			x = messages.pop(1)
-			tokens_estimated -= _token_count(x)
-		_summarize_converstation(messages)
+			print('Removing ', x['content'])
+			tokens_estimated -= x['tokens']
 	return messages
 
+# ------------------------------------------------------- Web Handling ------------------------------------------------------
 @app.route('/')
 def index():
 	_session_init()
@@ -89,7 +125,7 @@ def index():
 
 @app.route('/clear_history', methods=['POST'])
 def clear_history():
-	_converstation_history_clear()
+	_converstation_clear()
 	response = 'Chat history cleared.'
 	print(response)
 	return jsonify({'response': response})
@@ -109,27 +145,29 @@ def get_response():
 		if x =='verylarge':	response_tokens = 3500
 		x = request.form['temperature']
 		temperature = float(x)/10
-		user_instructions = request.form['user_instructions'][:max_question_length].strip()
 		
-		messages = []
 		system_command = "Be descriptive and if the response has more than 50 tokens then use HTML formatting"
+		#system_command = "Provide a detailed description of the issue or topic. If your response contains more than 50 tokens, use HTML formatting to ensure clarity. In the conversation history, the assistant's responses may be truncated to prioritize information the user provided."
+		system_tokens = len(system_command.split())
+		user_instructions = request.form['user_instructions'][:max_question_length].strip()
+		user_tokens = _token_count(user_instructions + '. ' + question)	
 		if user_instructions !='': system_command = user_instructions + '. ' + system_command
+		messages = []
 		messages.append({"role": "system", "content": system_command})
-		token_estimate = _token_count(system_command + ' ' + question) + response_tokens
+		token_estimate = system_tokens + user_tokens + response_tokens
 		if conversation_mode:	#add as much of the conversation history as possible without risking going over max_tokens
 			messages += _converstation_history_get(tokens_spoken_for=token_estimate)
-		else:
-			_converstation_history_clear()
 		messages.append({"role": "user", "content": question})
-		session['question_history'].append(question[:max_question_length])
+		_converstation_append(content=question, token_count=user_tokens, is_response=False)
 		print(messages)
-		response = _chatgpt_query(messages, response_tokens, temperature=temperature)
+		response = _chatgpt_query(messages, response_tokens, include_usage_summary=True, temperature=temperature)
 	return jsonify({'response': response})
 
 @app.route('/logout')
 def logout():
-    session.clear()
-    return 'Logged out successfully'
+	_converstation_clear()
+	session.clear()
+	return 'Logged out successfully'
 	
 if __name__ == '__main__':
 	app.run(debug=True)
